ecdb07c8668912f37d3dd214a424512c831c81a3
Melanie Roberts <robertsmelaniecc@gmail.com>
2023-01-31 23:29:27 -0500

    initial commit, not finished ran out of time

---
+++ script.js
@@ -0,0 +1,14 @@
+const progress = document.getElementById("progress");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const circles = document.querySelectorAll(".circles");
+
+let currentActive = 1;
+
+next.addEventListener("click", () => {
+  currentActive++;
+
+  if (currentActive > circles.length) {
+    currentActive = circles.length
+  }
+// Needed to stop for the night
