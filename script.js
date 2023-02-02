4f943ae9d14299d6c1a587fea49566bc8469131d
Melanie Roberts <robertsmelaniecc@gmail.com>
2023-02-01 22:05:47 -0500

    updating js file

--- script.js
+++ script.js
@@ -9,6 +9,40 @@ next.addEventListener("click", () => {
   currentActive++;
 
   if (currentActive > circles.length) {
-    currentActive = circles.length
+    currentActive = circles.length;
   }
-// Needed to stop for the night
+  update();
+});
+
+prev.addEventListener("click", () => {
+  currentActive--;
+
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+  update();
+});
+
+function update() {
+  circles.forEach((circle, idx) => {
+    if (idx < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+  });
+
+  const actives = document.querySelectorAll(".active");
+
+  progress.style.width =
+    ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+
+  if (currentActive === 1) {
+    prev.disabled = true;
+  } else if (currentActive === circles.length) {
+    next.disabled = true;
+  } else {
+    prev.disabled = false;
+    next.disabled = false;
+  }
+}
